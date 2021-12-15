5756570c0f7e05fac9cf81d6e26375cab16cd835
jimbo2106 <90231001+monerochan-moe@users.noreply.github.com>
2021-12-15 18:31:34 -0500

    added pepe speech bubble

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ function App() {
           <div style={{display:"flex", justifyContent:"right"}}>
             <a href="https://thetendiefren.co/" text-align="right" target="_blank" rel="noreferrer" style={{fontSize: "0px"}}>
               <img class="pepe" src="https://siasky.net/BAAjTZYF9KFBbs8eKJn7Kons1ArdcrWQ4WNlZc3EfSht_w" alt="Pepe" text-align="right"/>
+              <img class="speech" src="https://siasky.net/PANPmO5L0FFe11XrD-U2Aj7PGCiyYFrTbjFEyBy7k2Sung" alt="speech" text-align="right" style={{width:"auto",height:"112px"}}/>
             </a>
           </div>
           <header className="App-header">
